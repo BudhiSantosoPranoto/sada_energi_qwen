1266ec0c3a8ed08fe3ccdb916312e72c4fc2bfa9
Budhi Santoso Pranoto <budhi.santoso.pranoto@gmail.com>
2026-09-01 02:09:52 +0700

    Update navbar.tsx

--- src/components/site/navbar.tsx
+++ src/components/site/navbar.tsx
@@ -36,9 +36,9 @@ export function Navbar() {
       <div className="container-brand flex h-16 items-center justify-between gap-4 lg:h-20">
         {/* Logo */}
         <a href="#top" className="flex items-center group">
-          {/* TODO: pastikan file logo final bernama logo-sada-light.svg di public/ */}
+          {/* TODO: pastikan file logo final bernama logo-sada.png di public/ */}
           <img
-            src="/logo-sada-light.svg"
+            src="/logo-sada.png"
             alt="Logo Sada Energi"
             className="h-9 w-auto lg:h-11"
           />
